15e5bda73b0e3adc2f24e757fffe0044874da5df
jaeseokwoo <jaeseokw@gmail.com>
2026-02-03 01:39:21 +0900

    엔티티 클래스와 JPA 매핑 정보 분리

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.security:spring-security-core") // PasswordEncoder를 구현하기 위해 Spring Security의 core 기능만 가지고 오기 위해 사용
 	compileOnly("org.projectlombok:lombok")
 	developmentOnly("org.springframework.boot:spring-boot-docker-compose")
 	runtimeOnly("com.h2database:h2")
